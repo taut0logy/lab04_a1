a21ef77d1100cc54a005638582684e63a6df5812
taut0logy <103948698+taut0logy@users.noreply.github.com>
2023-09-02 14:37:46 +0600

    added new components

--- app/src/main/java/com/example/lab04/AboutActivity.java
+++ app/src/main/java/com/example/lab04/AboutActivity.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class BottomActivity extends AppCompatActivity {
+public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bottom);
-
+        setContentView(R.layout.activity_about);
     }
 }
